a1dd266af931c90c63448f9a2f241769686adaea
Fardjad Davari <public@fardjad.com>
2024-07-29 15:07:28 +0200

    Add Gravatar image link generator

---
+++ src/gravatar/avatar.test.ts
@@ -0,0 +1,35 @@
+import { describe, it } from "@std/testing/bdd";
+import { assertEquals, assertMatch } from "@std/assert";
+import { generateGravatarImageLink } from "./avatar.ts";
+
+describe("generateGravatarImageLink", () => {
+  it("should generate the correct Gravatar link with default size", async () => {
+    const email = "example@example.com";
+    const result = await generateGravatarImageLink(email);
+    const url = new URL(result);
+
+    assertMatch(
+      url.href,
+      /^https:\/\/www\.gravatar\.com\/avatar\/[0-9a-f]+/,
+    );
+    assertEquals(Object.fromEntries(url.searchParams.entries()), {
+      s: "64",
+      d: "robohash",
+    });
+  });
+
+  it("should generate the correct Gravatar link with custom size", async () => {
+    const email = "example@example.com";
+    const result = await generateGravatarImageLink(email, 128);
+    const url = new URL(result);
+
+    assertMatch(
+      url.href,
+      /^https:\/\/www\.gravatar\.com\/avatar\/[0-9a-f]+/,
+    );
+    assertEquals(Object.fromEntries(url.searchParams.entries()), {
+      s: "128",
+      d: "robohash",
+    });
+  });
+});
---
+++ src/gravatar/avatar.ts
@@ -0,0 +1,13 @@
+import { crypto } from "@std/crypto";
+import { encodeHex } from "@std/encoding/hex";
+
+export const generateGravatarImageLink = async (
+  email: string,
+  size: number = 64,
+) => {
+  const emailBuffer = new TextEncoder().encode(email);
+  const hashedEmail = encodeHex(
+    await crypto.subtle.digest("SHA-256", emailBuffer),
+  );
+  return `https://www.gravatar.com/avatar/${hashedEmail}?s=${size}&d=robohash`;
+};
