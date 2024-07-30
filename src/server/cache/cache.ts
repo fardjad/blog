7934e4a9d0e53ea0adaa6199697b8e9866a9d546
Fardjad Davari <public@fardjad.com>
2024-07-30 16:53:37 +0200

    Apply Deno Deploy specific changes
    Cache API is not supported
    Also added Deploy configuration to `deno.jsonc`

--- src/server/cache/cache.ts
+++ src/server/cache/cache.ts
@@ -1,12 +1,22 @@
 import { cache as honoCache } from "hono/cache";
+import { createFactory } from "hono/factory";
+
+const factory = createFactory();
 
 export const cacheKey = "blog";
-export const cache = honoCache({
-  cacheName: cacheKey,
-  cacheControl: "public, max-age=300",
-  wait: true,
-});
+
+// Deno Deploy does not support Cache API yet so we need to check if it is
+// available
+export const cache = globalThis.caches
+  ? honoCache({
+    cacheName: cacheKey,
+    cacheControl: "public, max-age=300",
+    wait: true,
+  })
+  : factory.createMiddleware(async (_c, next) => {
+    await next();
+  });
 
 export const clearCache = async () => {
-  await caches.delete(cacheKey);
+  await globalThis.caches?.delete(cacheKey);
 };
