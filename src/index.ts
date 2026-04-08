a1444ffcfd4790e9ec4d799a9b88206dd4f49eae
Fardjad Davari <public@fardjad.com>
2026-04-08 02:15:46 +0200

    Run the cronjob every hour

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { client } from "./database/client.ts";
 import { createApp } from "./server/app.ts";
 
 // On Deno Deploy, the cron job will be executed in a separate process.
-Deno.cron("Sync posts", "* * * * *", syncPosts);
+Deno.cron("Sync posts", "0 * * * *", syncPosts);
 
 // Run the job immediately on startup to have some posts available right away.
 await syncPosts();
